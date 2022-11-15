e836e6829be92b82677fbcd6e9f60fdf6d709620
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-11-15 20:49:11 +0200

    add files

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,19 +1,1 @@
-import Container from 'components/Container';
-import DailyCaloriesForm from 'components/DailyCaloriesForm';
 
-const CalculatorPage = () => {
-  return (
-    <Container>
-      <div>
-        <h1>Calculate your daily calorie intake right now</h1>
-        <DailyCaloriesForm />
-      </div>
-      {/* <RightSideBar
-          date={date}
-          userParams={userParams}
-          userProducts={userProducts}
-        /> */}
-    </Container>
-  );
-};
-export default CalculatorPage;
