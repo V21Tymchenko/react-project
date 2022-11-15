c7d6921ea5b87e65684a2fffe30c19ebdfb3f666
VTrofym <v.trof@icloud.com>
2022-11-15 18:13:28 +0200

    added registrationForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
+import RegisterPage from 'pages/RegisterPage';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div>Home</div>}>
-        <Route index element={<div>Home</div>} />
-        <Route path="register" element={<div>REGISTER</div>} />
-        <Route path="login" element={<div>LOGIN</div>} />
-        <Route path="calculator" element={<div>CALCULATOR</div>} />
-        <Route path="diary" element={<div>DIARY</div>} />
-      </Route>
-    </Routes>
+    <>
+      <RegisterPage />
+      <Routes>
+        <Route path="/" element={<div>Home</div>}>
+          <Route index element={<div>Home</div>} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<div>LOGIN</div>} />
+          <Route path="calculator" element={<div>CALCULATOR</div>} />
+          <Route path="diary" element={<div>DIARY</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 };
