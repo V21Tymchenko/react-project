b867874b9dc2e7901f1d2cb83bb82a39f556ae62
VTrofym <v.trof@icloud.com>
2022-11-15 19:20:22 +0200

    added LoginForm and LoginPage

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,15 @@
 import Container from 'components/Container';
 import Header from 'components/Header';
+import { Outlet } from 'react-router-dom';
 
 const MainPage = () => {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <Outlet />
+    </>
   );
 };
 export default MainPage;
--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,6 +1,15 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+export const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
 export async function registerNewUser(user) {
   const { data } = await axios.post(
     'https://slimmom-backend.goit.global/auth/register',
@@ -13,10 +22,27 @@ export const register = createAsyncThunk(
   async (data, thunkAPI) => {
     try {
       const response = await registerNewUser(data);
-      console.log('response :>> ', response);
       return response;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
+
+export async function loginUser(user) {
+  const { data } = await axios.post(
+    'https://slimmom-backend.goit.global/auth/login',
+    user
+  );
+  return data;
+}
+export const login = createAsyncThunk('auth/login', async (data, thunkAPI) => {
+  try {
+    const response = await loginUser(data);
+    token.set(response.token);
+    console.log('response :>> ', response);
+    return response;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
