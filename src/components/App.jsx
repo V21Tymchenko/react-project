c9fa074ec29f82a3feab8a0e75a47c6a4436207c
makslusak <106471649+makslusak@users.noreply.github.com>
2022-11-15 18:50:49 +0200

    Fix route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />}>
-        <Route index element={<div>Home</div>} />
+      <Route path="/">
+        <Route index element={<MainPage />} />
         <Route path="register" element={<div>REGISTER</div>} />
         <Route path="login" element={<div>LOGIN</div>} />
         <Route path="calculator" element={<div>CALCULATOR</div>} />
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,14 +1,18 @@
 import Container from 'components/Container';
 import DailyCaloriesForm from 'components/DailyCaloriesForm';
 import Header from 'components/Header';
+import { Outlet } from 'react-router-dom';
 
 const MainPage = () => {
   return (
-    <Container>
-      <Header />
-      <h1>Calculate your daily calorie intake right now</h1>
-      <DailyCaloriesForm />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <h1>Calculate your daily calorie intake right now</h1>
+        <DailyCaloriesForm />
+      </Container>
+      <Outlet />
+    </>
   );
 };
 export default MainPage;
