82021e6223118712359c0cdb3ff7522a26136cc5
VTrofym <v.trof@icloud.com>
2022-11-18 14:11:04 +0200

    added tosty

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useSelector } from 'react-redux';
 import {
   dailyRateSel,
@@ -57,4 +58,28 @@ const RightSideBar = () => {
   );
 };
 
+=======
+const RightSideBar = () => {
+  return (
+    <div className="sideBar">
+      <h2>Summary for 06/20/2020</h2>
+      <p>
+        Left<span>000 kcal</span>
+      </p>
+      <p>
+        Consumed<span>000 kcal</span>
+      </p>
+      <p>
+        Daily rate<span>000 kcal</span>
+      </p>
+      <p>
+        n% of normal<span>000 kcal</span>
+      </p>
+      <h2>Food not recommended</h2>
+      <ul>{/* map(item => {<li>item</li>}) */}</ul>
+    </div>
+  );
+};
+
+>>>>>>> Stashed changes
 export default RightSideBar;
