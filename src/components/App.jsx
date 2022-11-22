e7d293383fdcdb6bd693d995ee4bf1173cc9e349
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-11-22 10:23:07 +0200

    add relise file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,24 @@
-// import CalculatorPage from 'pages/CalculatorPage';
-// import RegisterPage from 'pages/RegisterPage';
-// import MainPage from 'pages/MainPage';
 import { Route, Routes } from 'react-router-dom';
-// import DiaryPage from 'pages/DiaryPage';
 import { useDispatch, useSelector } from 'react-redux';
 import { Suspense, useEffect } from 'react';
-
 import { stateAuthToken } from 'redux/auth/auth-selectors';
-// import LoginPage from 'pages/LoginPage/LoginPage';
 import PublicRoute from './PublicRoute/PublicRoute';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
-
-import { useMediaQuery } from 'react-responsive';
 import NotFound from 'pages/NotFound/NotFound';
-
 import { lazy } from 'react';
 import { current } from 'redux/user/user-operation';
-// import Loader from './Loader';
-import Header from './Header';
+
+import Header from './Header/Header';
 
 const MainPage = lazy(() => import('pages/MainPage'));
 const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
 const CalculatorPage = lazy(() => import('pages/CalculatorPage'));
 const DiaryPage = lazy(() => import('pages/DiaryPage'));
-const MobilNavPage = lazy(() => import('pages/MobilNavPage/MobilNavPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
   const token = useSelector(stateAuthToken);
-  const isDesctop = useMediaQuery({ query: '(min-width: 1280px)' });
 
   useEffect(() => {
     if (token) {
